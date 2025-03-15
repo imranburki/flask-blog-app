e9a4b2964e5d19747fd32364c710a272c1a4b7ee
imranburki <imranburki438@gmail.com>
2025-03-16 03:26:41 +0500

    task 1.1 completed

--- app.py
+++ app.py
@@ -48,6 +48,6 @@ def blog_post(post_id):
 def page_not_found(error):
     return render_template('404.html'), 404
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
